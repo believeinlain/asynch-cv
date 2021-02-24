f87e4db2c0a4d34e8e22be4722861642ba23ed33
Stephanie Aelmore <steph.aelmore@gmail.com>
2021-02-23 16:58:22 -1000

    Added adaptive spatial filter

--- event_processing/__init__.py
+++ event_processing/__init__.py
@@ -3,3 +3,4 @@ from event_processing.basic_consumer import basic_consumer
 from event_processing.filter_consumer import filter_consumer
 
 from event_processing.basic_filter import basic_filter
+from event_processing.spatial_filter import spatial_filter
---
+++ event_processing/spatial_filter.py
@@ -0,0 +1,61 @@
+
+import numpy as np
+
+class spatial_filter:
+    '''
+    '''
+
+    def __init__(self, width, height, threshold):
+        self.dt = threshold
+        self.width = width
+        self.height = height
+        self.surface = np.zeros((width, height), dtype=np.uint)
+    
+    def is_event_allowed(self, event):
+        '''
+        Determines if an event is filtered out.
+
+        Parameters:
+            event: tuple or array or list in the form ('x','y','p','t')
+
+        Returns:
+            True if the event should be allowed, False if the event should be filtered out
+        '''
+        # unpack for readability
+        (x, y, p, t) = event
+        # p is currently unused
+        del p
+
+        # temporal pre-filtering before updating surface
+        if self.surface[x, y] + self.dt < t:
+
+            nearby_depth = 100_000
+            neighbor_depth = 10_000
+            nearby_range = 1
+            neighbor_range = 3
+            min_nearby = 2
+            neighbor_effect = 0.5
+            
+            def within_threshold(x, th):
+                return x > t-th
+            
+            # query nearby for surrounding events
+            row = np.arange(x-nearby_range, x+nearby_range+1).clip(0, self.width-1)
+            col = np.arange(y-nearby_range, y+nearby_range+1).clip(0, self.height-1)
+
+            nearby_count = np.sum(within_threshold(self.surface[row[:, np.newaxis], col], nearby_depth))
+
+            # query neighborhood for surrounding events
+            row = np.arange(x-neighbor_range, x+neighbor_range+1).clip(0, self.width-1)
+            col = np.arange(y-neighbor_range, y+neighbor_range+1).clip(0, self.height-1)
+
+            neighbor_count = np.sum(within_threshold(self.surface[row[:, np.newaxis], col], neighbor_depth))
+
+            # update the surface
+            self.surface[x, y] = t
+
+            if nearby_count > neighbor_count*neighbor_effect + min_nearby:
+                return True
+
+        else:
+            return False
